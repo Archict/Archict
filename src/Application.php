817e0fe02da4ba5699ce276c8094e77186c0014b
Kevin Traini <kevin@ktraini.com>
2024-05-28 18:50:59 +0200

    feat: Add 2 routes -> / and /status
    Part of #1

---
+++ src/Application.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024-Present Kevin Traini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Archict\Archict;
+
+use Archict\Archict\Controller\HomeController;
+use Archict\Archict\Controller\StatusController;
+use Archict\Brick\ListeningEvent;
+use Archict\Brick\Service;
+use Archict\Router\Method;
+use Archict\Router\RouteCollectorEvent;
+
+#[Service]
+final readonly class Application
+{
+    #[ListeningEvent]
+    public function collectRoutes(RouteCollectorEvent $collector): void
+    {
+        $collector->addRoute(Method::GET, '', new HomeController());
+        $collector->addRoute('GET', '/status', new StatusController());
+    }
+}
